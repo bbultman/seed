b53ad6e2be00a2d96931704dce5f2aec3cfadbad
Benjamin Bultman <bultman.ben@gmail.com>
2018-12-20 11:25:06 +0100

    Remove unnecessary examples

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,3 @@
 module.exports = {
-  demo: require('./demo'),
   health: require('./health')
 }
--- src/routes/v1/index.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router()
-const { demo } = require('../../api')
-
-router.use('/demo', demo.index)
-
-module.exports = router
